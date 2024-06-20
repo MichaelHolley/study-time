e807c9239c4550a800a1fca6419af35fc8bbd168
Michael Holley <michael.philipp.holley@gmail.com>
2024-06-20 21:25:25 +0200

    Initial commit

---
+++ study-time-web/src/lib/timer.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let timeInSeconds = 50 * 60;
+
+	setInterval(() => {
+		timeInSeconds -= 1;
+	}, 1000);
+
+	$: hours = Math.floor(timeInSeconds / 3600);
+	$: minutes = Math.floor((timeInSeconds % 3600) / 60);
+	$: seconds = timeInSeconds % 60;
+
+	function transformToDisplay(number: number): string {
+		if (number < 10) {
+			return `0${number}`;
+		} else {
+			return `${number}`;
+		}
+	}
+</script>
+
+<span class="font-bold text-7xl text-white">
+	{#if !!hours && hours > 0}<span>{transformToDisplay(hours)}</span><span>:</span>{/if}
+	<span>{transformToDisplay(minutes)}</span>
+	<span>:</span>
+	<span>{transformToDisplay(seconds)}</span>
+</span>
---
+++ study-time-web/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Timer from '$lib/timer.svelte';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Playwrite+ES:wght@100..400&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<div class="flex flex-col items-center justify-center h-screen bg-red-400">
+	<Timer></Timer>
+</div>
+
+<style>
+	* {
+		font-family: 'Playwrite ES';
+	}
+</style>
