bcfa68717c7c9dd99ec2e1c524d69f7337ed1fd4
Michael Holley <michael.philipp.holley@gmail.com>
2024-10-22 11:17:19 +0200

    resolving minor display issue

--- src/lib/progressbar.svelte
+++ src/lib/progressbar.svelte
@@ -8,6 +8,7 @@
 		if (remaining === 0 && progressBar) {
 			progressBar.classList.remove('progress-bar');
 			progressBar.style.animationDuration = `0s`;
+			progressBar.style.width = '100%';
 		}
 
 		if (remaining > 0 && progressBar && !progressBar.classList.contains('progress-bar')) {
@@ -18,7 +19,7 @@
 </script>
 
 <div class="rounded-full bg-red-500 h-2 w-full">
-	<div bind:this={progressBar} class="rounded-full bg-gray-200 h-2"></div>
+	<div bind:this={progressBar} class="w-0 rounded-full bg-gray-200 h-2"></div>
 </div>
 
 <style>
